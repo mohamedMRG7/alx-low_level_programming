88f87355dfb80816c873821b07a7dfeb24ba1cdf
mohamedMRG <mohamad1325@gmail.com>
2023-06-17 08:18:43 -0700

    digits compinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,2 +1,32 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int s;
+
+	for (i = 48; i <= 57 ; i++)
+	{
+		for (s = 48; s <= 57 ; s++)
+		{
+			putchar(i);
+			putchar(s);
+			if (i != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+
+		}
+	}
+	return (0);
+}
