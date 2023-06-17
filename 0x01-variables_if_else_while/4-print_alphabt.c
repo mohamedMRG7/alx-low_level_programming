e43596c17c8707ea6973bf5d5ce639f11ab56fd4
mohamedMRG <mohamad1325@gmail.com>
2023-06-17 07:51:09 -0700

    all alpha ecept q,e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,2 +1,24 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122 ; i++)
+	{
+		if (i != 113 && i != 101)
+			putchar(i);
+		if (i == 122)
+			putchar(10);
+	}
+	return (0);
+}
