2dfcf3e6eef74ca751aa283c110c39b495b5bdc6
mohamedMRG <mohamad1325@gmail.com>
2023-06-17 08:03:45 -0700

    print hexadecimal numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,2 +1,31 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int s;
+
+	for (i = 48; i <= 57 ; i++)
+	{
+		putchar(i);
+		if (i == 57)
+			for (s = 97; s <= 102; s++)
+			{
+				putchar(s);
+				if (s == 90)
+				{
+					putchar(10);
+				}
+			}
+	}
+	return (0);
+}
