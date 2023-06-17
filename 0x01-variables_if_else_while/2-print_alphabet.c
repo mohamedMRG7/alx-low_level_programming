fad51edc2a58a754c51f5ef57d5b85352096d6e2
mohamedMRG <mohamad1325@gmail.com>
2023-06-17 07:30:58 -0700

    alphabatic chars

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,2 +1,20 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122 ; i++)
+		putchar(98);
+
+	return (0);
+}
