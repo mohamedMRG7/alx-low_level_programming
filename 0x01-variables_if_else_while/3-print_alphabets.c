258c4d8497b0957c5c8c09c713ff0cef870f66a6
mohamedMRG <mohamad1325@gmail.com>
2023-06-17 07:47:02 -0700

    small and capital aphabits

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,30 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122 ; i++)
+	{
+		putchar(i);
+		if (i == 122)
+			for (int s = 65; s <= 90; s++)
+			{
+				putchar(s);
+				if (s == 90)
+				{
+					putchar(10);
+				}
+			}
+	}
+	return (0);
+}
