7954a3a8c22997dde71e8b53a85c046ce63be676
mohamedMRG <mohamad1325@gmail.com>
2023-06-17 21:19:55 -0700

    number combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int s;
+
+	for (i = 48; i <= 57 ; i++)
+	{
+		for (s = 48; s <= 57; s++)
+		{
+			putchar(i);
+			putchar(s);
+			if (i != 57 || s != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		if (i == 57)
+			putchar(10);
+	}
+	return (0);
+}
