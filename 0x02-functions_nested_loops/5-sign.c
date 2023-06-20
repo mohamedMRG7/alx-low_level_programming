619db94d350157c2d67218b1c35b4d322e94a3c8
mohamedMRG <mohamad1325@gmail.com>
2023-06-20 12:31:53 -0700

    print sighn

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,18 +5,23 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
- * _isalpha - print alphabit
- * @c: is input
+ * print_sign - print alphabit
+ * @n: is input
  * Return: 0 if upper or 1 if lower
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
-	}else if(n == 0 )
+	} else if (n == 0)
 	{
+		_putchar('0');
 		return (0);
-	}else
+	} else if (n < 0)
+	{
+		_putchar('-');
 		return (-1);
+	}
 }
