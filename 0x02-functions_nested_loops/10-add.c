b978b301c1f3951db5c0da0417543a20f5209ec6
mohamedMRG <mohamad1325@gmail.com>
2023-06-20 14:30:17 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+#include <limits.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * print_last_digit - print alphabit
+ * @n: is input
+ * Return: 0 if upper or 1 if lower
+ */
+int add(int x, int y)
+{
+	return (x+y);
+}
