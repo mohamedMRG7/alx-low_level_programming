bff43c7638d0fe808f5888db2163abe344bac10d
mohamedMRG <mohamad1325@gmail.com>
2023-06-20 13:41:04 -0700

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -17,7 +17,10 @@ int print_last_digit(int n)
 	if (n < 0)
 		n *= -1;
 	if (n == INT_MIN)
-		n = -2147483648;
+	{
+		_putchar('0' + 8);
+		return (8);
+	}
 	i = n % 10;
 	_putchar('0' + i);
 	return (i);
