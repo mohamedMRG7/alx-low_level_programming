697b8e94f1947db22d3156ecfced76e4dc24e8a4
mohamedMRG <mohamad1325@gmail.com>
2023-06-20 14:19:20 -0700

    dayhours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,6 +17,8 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
+			if (minute < 10)
+				_putchar('0');
 			_putchar("0" + hour);
 			_putchar(':');
 			_putchar("0" + minute);
