e846979e224eb6aaef0c2e3698c15fb4299b8153
mohamedMRG <mohamad1325@gmail.com>
2023-06-20 13:06:05 -0700

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * print_last_digit - print alphabit
+ * @n: is input
+ * Return: 0 if upper or 1 if lower
+ */
+int print_last_digit(int n)
+{
+	n % = 10;
+	return (n);
+}
