823c6505314aefb8d2c6cb360d75c33d6b179e42
mohamedMRG <mohamad1325@gmail.com>
2023-06-20 05:41:56 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "_putchar.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('c');
+	return (0);
+}
