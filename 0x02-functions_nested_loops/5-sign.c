5f96340562e3ce52f4d2d958461344473f701b8d
mohamedMRG <mohamad1325@gmail.com>
2023-06-20 12:23:27 -0700

    print sighn

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,2 +1,22 @@
-#!/bin/bash
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * _isalpha - print alphabit
+ * @c: is input
+ * Return: 0 if upper or 1 if lower
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}else if(n == 0 )
+	{
+		return (0);
+	}else
+		return (-1);
+}
