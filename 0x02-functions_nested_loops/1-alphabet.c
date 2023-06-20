97c6554ea513eca1d408d6739b4569b7ccaab2d7
mohamedMRG <mohamad1325@gmail.com>
2023-06-20 06:24:17 -0700

    alphabit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,23 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c[] = "abcdfghijklmnoprstuvwxyz\n";
+	int i;
+
+	for (i = 0; i < 25; i++)
+	{
+		_putchar(c[i]);
+	}
+	return (0);
+}
