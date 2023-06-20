92f2e4114e1fbdd729a2c7c631ca2f7341f2ccce
mohamedMRG <mohamad1325@gmail.com>
2023-06-20 14:13:26 -0700

    dayhours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,2 +1,25 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+#include <limits.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * jack_bauer - print alphabit
+ * Return: 0 if upper or 1 if lower
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
 
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar(hour);
+			_putchar(':');
+			_putchar(minute);
+		}
+	}
+}
