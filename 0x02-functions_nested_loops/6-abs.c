80d13597472c94d074cbe77e11059b7500e639fb
mohamedMRG <mohamad1325@gmail.com>
2023-06-20 13:02:22 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,2 +1,19 @@
-#!/bin/bash
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * _abs - print alphabit
+ * @n: is input
+ * Return: 0 if upper or 1 if lower
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n* = -1;
+	}
+	return (n);
+}
