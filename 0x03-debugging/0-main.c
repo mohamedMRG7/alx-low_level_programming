f81542029ed83cef34496109b753aedbce2f5acc
mohamedMRG <mohamad1325@gmail.com>
2023-06-21 12:35:53 -0700

    debuging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 int i;
+	 
+	 i = 98;
+	 positive_or_negative(i);
+
+	return (0);
+}
