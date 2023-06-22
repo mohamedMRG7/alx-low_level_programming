12220766ee09c7356c4f71ceab830eed0c073f72
mohamedMRG <mohamad1325@gmail.com>
2023-06-22 08:58:04 -0700

    first

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * _isalpha - print alphabit
+ * @c: is input
+ * Return: 0 if upper or 1 if lower
+ */
+int _isupper(int c)
+{
+	return (isupper(c));
+}
