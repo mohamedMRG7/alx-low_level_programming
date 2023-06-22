bdd3ac798802596220080227233af219f8d85dbc
mohamedMRG <mohamad1325@gmail.com>
2023-06-22 07:53:06 -0700

    test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,21 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 #include "main.h"
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
-	 int i;
-	 
-	 i = 98;
-	 positive_or_negative(i);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
