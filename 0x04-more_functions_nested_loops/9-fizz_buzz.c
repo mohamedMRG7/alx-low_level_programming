41be756e25f03898ab0344b201ab405f84d0d9ee
mohamedMRG <mohamad1325@gmail.com>
2023-06-23 13:26:43 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,6 +5,17 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
  * fizz - print alphabit
  * Return: 0 if upper or 1 if lower
  */
