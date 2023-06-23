c2ba6905d43d11d42863daa06e5d69d887b1b5a7
mohamedMRG <mohamad1325@gmail.com>
2023-06-23 08:58:44 -0700

    diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,2 +1,40 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * printspace - print alphabit
+ *@n: is input
+ * Return: 0 if upper or 1 if lower
+ */
+void printspace(int n)
+{
+	while (n)
+	{
+		_putchar(' ');
+		n--;
+	}
+}
+/**
+ * print_line - print alphabit
+ *@n: is input
+ * Return: 0 if upper or 1 if lower
+ */
+void print_diagonal(int n);
+{
+	int i;
 
+	if (n > 0)
+	{
+		for (i = 1; i <= n ; i++)
+		{
+			printspace(i);
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
