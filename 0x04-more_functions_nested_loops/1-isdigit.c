7b9520caae18b142f6ab3d632f1e9bd627eb1248
mohamedMRG <mohamad1325@gmail.com>
2023-06-23 05:52:03 -0700

    numbers

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,2 +1,22 @@
-#!/bin/bash
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * _isdigit - print alphabit
+ * @c: is input
+ * Return: 0 if upper or 1 if lower
+ */
+int _isdigit(int c)
+{
+	for (i = 0; i < 10; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
