936531c05cda123dfb23601ec1a0e1988d2a1684
mohamedMRG <mohamad1325@gmail.com>
2023-06-23 08:06:58 -0700

    nm

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,25 +5,34 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
+ * pn - print alphabit
+ * Return: 0 if upper or 1 if lower
+ */
+void pn(void)
+{
+	int i;
+
+	for (i = 0; i <= 15; i++)
+	{
+		if (i <= 9)
+			_putchar('0' + i);
+		else
+		{
+			_putchar(i < 15 ? '0' + i / 10 : '\n');
+			if (i < 15)
+				_putchar('0' + i % 10);
+		}
+
+	}
+}
+/**
  * more_numbers - print alphabit
  * Return: 0 if upper or 1 if lower
  */
 void more_numbers(void)
 {
 	int i;
-	int c;
 
-	for (c = 0; c <= 10; c++)
-		for (i = 0; i <= 14; i++)
-		{
-			if (i <= 9)
-				_putchar('0' + i);
-			else
-			{
-				_putchar('0' + i / 10);
-				_putchar('0' + i % 10);
-			}
-			if (i == 14)
-				_putchar('\n');
-		}
+	for (i = 0; i <= 10; i++)
+		pn();
 }
