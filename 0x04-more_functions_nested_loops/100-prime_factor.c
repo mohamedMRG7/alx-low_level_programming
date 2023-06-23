e04eea551f98fcce3005b0e03da8c18eb9b78b82
mohamedMRG <mohamad1325@gmail.com>
2023-06-23 15:18:08 -0700

    prim

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,6 +5,29 @@
 #include <unistd.h>
 /* more headers goes there */
 /* betty style doc for function main goes there */
+/**
+ * print - print alphabit
+ *@number : d
+ * Return: 0 if upper or 1 if lower
+ */
+void print(int number)
+{
+	char buffer[20];
+	int i = 0;
+	int j = 0;
+
+	do {
+		buffer[i++] = (number % 10) + '0';
+		number /= 10;
+	} while (number > 0);
+	for (j = i - 1; j >= 0; j--)
+		_putchar(buffer[j]);
+}
+/**
+ * _putchar - print alphabit
+ *@c : d
+ * Return: 0 if upper or 1 if lower
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
@@ -22,16 +45,17 @@ int main(void)
 	i = 2;
 	num = 612852475143;
 	prim = 0;
-	while (num /i != 1)
+	while (num / i != 1)
 	{
 		if (num % i == 0)
 		{
 			num = num / i;
 			if (i > prim)
 				prim = i;
-		}else
+		} else
 			i++;
 	}
-	printf("%d\n",6857);
+	print(prim);
+	_putchar('\n');
 	return (0);
 }
