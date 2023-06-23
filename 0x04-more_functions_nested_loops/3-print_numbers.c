c724cc5bf5c342a5b418071e8585382db7e03c3e
mohamedMRG <mohamad1325@gmail.com>
2023-06-23 06:15:54 -0700

    print nubmer

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,2 +1,17 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * print_numbers - print alphabit
+ * Return: 0 if upper or 1 if lower
+ */
+void print_numbers(void)
+{
+	int i;
 
+	for (i = 0; i <= 9; i++)
+		_putchar(i);
+}
