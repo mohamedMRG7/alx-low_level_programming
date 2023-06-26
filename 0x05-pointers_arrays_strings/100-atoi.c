d65f287ca4284f92e81d4ad6de0b4b08c7234fdd
mohamedMRG <mohamad1325@gmail.com>
2023-06-26 14:24:11 -0700

    num

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,27 +8,6 @@
 */
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-	int i = 0;
 
-	while (s[i] == ' ')
-		i++;
-	if (s[i] == '-' || s[i] == '+') {
-		sign = (s[i] == '-') ? -1 : 1;
-		i++;
-	}
-	while (s[i] >= '0' && s[i] <= '9') {
-		int digit = s[i] - '0';
-
-		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)) {
-			if (sign == 1)
-				return INT_MAX;
-			else
-				return INT_MIN;
-		}
-		result = result * 10 + digit;
-		i++;
-	}
-	return sign * result;
+	return (0);
 }
