2afef01f407e5c1cabad3ab66f608261c703b10c
mohamedMRG <mohamad1325@gmail.com>
2023-06-30 08:20:33 -0700

    strcopyy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,11 +2,26 @@
 #include <stdio.h>
 
 /**
-* main - check the code
-*
+* _strncpy - check the code
+* @dest :d
+* @src :s
+* @n :n
 * Return: Always 0.
 */
-int main(void)
+char *_strncpy(char *dest, char *src, int n)
 {
-    return (0);
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
