18e9aeb365a22ef4b9b0084afefc804bf124f2ba
mohamedMRG <mohamad1325@gmail.com>
2023-07-05 08:22:36 -0700

    Puts with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,12 @@
 */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	if (s[0] == '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(s[0]);
+	s = s + 1;
+	_puts_recursion(s);
 }
