62a65af6c77b312d35e0e3b41bed37bedfebf9b9
mohamedMRG <mohamad1325@gmail.com>
2023-07-05 16:01:47 -0700

    prim

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -1,9 +1,8 @@
+#include "main.h"
 #include <stdio.h>
 
-int _sqrt_recursion(int);
-
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -11,8 +10,21 @@ int main(void)
 {
 	int r;
 
-
-	r = _sqrt_recursion(16777216);
+	r = is_prime_number(1);
+	printf("%d\n", r);
+	r = is_prime_number(1024);
+	printf("%d\n", r);
+	r = is_prime_number(16);
+	printf("%d\n", r);
+	r = is_prime_number(17);
+	printf("%d\n", r);
+	r = is_prime_number(25);
+	printf("%d\n", r);
+	r = is_prime_number(-1);
+	printf("%d\n", r);
+	r = is_prime_number(113);
+	printf("%d\n", r);
+	r = is_prime_number(7919);
 	printf("%d\n", r);
 	return (0);
 }
