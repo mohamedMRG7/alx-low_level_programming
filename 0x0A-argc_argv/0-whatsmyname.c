ad1d32146e027f3cd155481fae42001828d2d1a7
mohamedMRG <mohamad1325@gmail.com>
2023-07-07 07:28:38 -0700

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - check the code
+* @argc:argc
+* @argv : argv
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
