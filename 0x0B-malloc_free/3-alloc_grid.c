9156daf08991e5bf4e6d15b2d8c60e16892fad66
mohamedMRG <mohamad1325@gmail.com>
2023-07-10 08:42:09 -0700

    two dimarray

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,15 +10,21 @@
 */
 int **alloc_grid(int width, int height)
 {
-	int *arr;
+	int **arr;
 	int i;
 	int j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	(*arr)[height] = (int *)malloc(sizeof(int[width][height]));
-	if (arr == NULL || *arr == NULL)
+	arr = (int **)malloc(sizeof(int *) * width);
+	if (arr == NULL)
 		return (NULL);
+	for  (i = 0; i < width; i++)
+	{
+		arr[i] = (int *)malloc(sizeof(int) * height);
+		if (*(arr + i) == NULL)
+			return (NULL);
+	}
 	for (i = 0; i < width; i++)
 		for (j = 0; j < height; j++)
 			arr[i][j] = 0;
