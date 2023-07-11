82dddae2ae9cf649bf3a2bd840a1fc683526b8e3
mohamedMRG <mohamad1325@gmail.com>
2023-07-11 11:38:33 -0700

    split

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,55 +1,20 @@
 #include <stdlib.h>
 #include "main.h"
 #include <stdio.h>
-
 /**
-* strtow - check the code
-* @str : size
-* Return: Always 0.
-*/
-char **strtow(char *str)
+ * setarratdata - check the code
+ * @str : size
+ * @rows : rows
+ * @arr : arr
+ * Return: Always 0.
+ */
+void setarratdata(int rows, char *str, char **arr)
 {
-	char **arr;
-	int i;
-	int size;
-	int rows;
 	int j;
 	int coulmn;
 	int isfirst;
+	int i;
 
-	rows = 0;
-	size = 0;
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] != ' ')
-			size++;
-		if (str[i] == ' ' && str[i + 1] != '\0' && str[i + 1] != ' ')
-			rows++;
-	}
-	if (str == NULL || rows == 0)
-		return (NULL);
-	arr = malloc(sizeof(char **) * rows + 1);
-	coulmn = 0;
-	rows = 0;
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == ' ')
-			continue;
-		if (str[i] != ' ')
-			coulmn++;
-		if (str[i + 1] == ' ' || str[i + 1] == '\0')
-		{
-			arr[rows] = malloc(sizeof(char) * coulmn);
-			coulmn = 0;
-			rows++;
-		}
-	}
-	arr[rows] = malloc(sizeof(char *) + 14);
-	if (arr == NULL)
-	{
-		free(arr);
-		return (NULL);
-	}
 	j = 0;
 	coulmn = 0;
 	isfirst = 1;
@@ -81,6 +46,58 @@ char **strtow(char *str)
 		}
 		coulmn = 0;
 	}
+}
+/**
+ * getrows - check the code
+ * @str : size
+ * Return: Always 0.
+ */
+int getrows(char *str)
+{
+	int rows;
+	int i;
+
+	rows = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' && str[i + 1] != '\0' && str[i + 1] != ' ')
+			rows++;
+	}
+	return (rows);
+}
+/**
+ * strtow - check the code
+ * @str : size
+ * Return: Always 0.
+ */
+char **strtow(char *str)
+{
+	char **arr;
+	int rows;
+	int coulmn;
+	int i;
+
+	rows = getrows(str);
+	if (str == NULL || rows == 0)
+		return (NULL);
+	arr = malloc(sizeof(char **) * rows + 1);
+	coulmn = 0;
+	rows = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+			continue;
+		if (str[i] != ' ')
+			coulmn++;
+		if (str[i + 1] == ' ' || str[i + 1] == '\0')
+		{
+			arr[rows] = malloc(sizeof(char) * coulmn);
+			coulmn = 0;
+			rows++;
+		}
+	}
+	arr[rows] = malloc(sizeof(char *) + 14);
+	setarratdata(rows, str, arr);
 	arr[rows] = NULL;
 	return (arr);
 }
