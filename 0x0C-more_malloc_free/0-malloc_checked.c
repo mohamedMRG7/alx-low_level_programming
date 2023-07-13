f32da7e3ec88219c1fd305769289ad2134247808
mohamedMRG <mohamad1325@gmail.com>
2023-07-13 07:43:04 -0700

    address

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+* malloc_checked - check the code
+* @b : b
+* Return: Always 0.
+*/
+void *malloc_checked(unsigned int b)
+{
+	return malloc(b);
+}
