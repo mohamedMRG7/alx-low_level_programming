7dc875d26ff596682e2f6e124ea05a15c57d2663
mohamedMRG <mohamad1325@gmail.com>
2023-07-17 06:32:38 -0700

    sd

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,9 +9,7 @@
  * @name: Pointer to a string representing the dog's name
  * @age: Age of the dog
  * @owner: Pointer to a string representing the dog's owner
- * Return  : 0
- * Description: This function initializes a dog structure with the given
- *              name, age, and owner.
+ * Return: 0
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -23,9 +21,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	d->name = malloc(strlen(name) + 1);
+	if (d->name == NULL)
+		return (NULL);
 	strcpy(d->name, name);
 	d->age = age;
 	d->owner = malloc(strlen(owner) + 1);
+	if (d->owner == NULL)
+		return (NULL);
 	strcpy(d->owner, owner);
 	return (d);
 }
