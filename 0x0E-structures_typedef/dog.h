823f9b6c51aba344c8679ebc1d35fbd99d4ed3be
mohamedMRG <mohamad1325@gmail.com>
2023-07-17 05:06:08 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,17 @@
-/*
- * dog : dog
- */
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - Structure representing a dog
+ * @name: Pointer to a string representing the dog's name
+ * @age: Age of the dog
+ * @owner: Pointer to a string representing the dog's owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 #endif
