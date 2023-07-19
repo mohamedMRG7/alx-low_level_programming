45b2aabba849afaa42046767627f3b6fec0b277a
mohamedMRG <mohamad1325@gmail.com>
2023-07-19 05:40:26 -0700

    func

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_name - check the code
+* @name:n
+*@f:f
+* Return: Always 0.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
