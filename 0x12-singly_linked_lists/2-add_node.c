572e838a344fb6b504fcf13aeeb4cfbc99e695d0
mohamedMRG <mohamad1325@gmail.com>
2023-07-27 13:27:56 -0700

    task

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,6 +15,8 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *node;
 
 	node = malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
 	node->str = strdup(str);
 	node->len = strlen(str);
 	node->next = *head;
