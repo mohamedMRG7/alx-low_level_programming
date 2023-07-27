9e59741f602552e9b7646b132bd20cf5ea030365
mohamedMRG <mohamad1325@gmail.com>
2023-07-27 14:10:25 -0700

    task

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,10 +11,8 @@
 */
 void free_list(list_t *head)
 {
-	while (head)
-	{
+	if (head->next != NULL)
 		free_list(head->next);
-		free(head->str);
-		free(head);
-	}
+	free(head->str);
+	free(head);
 }
