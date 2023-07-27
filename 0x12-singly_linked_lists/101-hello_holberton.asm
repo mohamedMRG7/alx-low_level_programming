3d13cd3ee91169c2c842fbd17afd7176659d27ef
mohamedMRG <mohamad1325@gmail.com>
2023-07-27 14:31:10 -0700

    task

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,24 @@
+	section .data
+	hello_msg db "Hello, Holberton", 0
+	format db "%s", 0
+
+	section .text
+	global _start
+
+_start:
+	;;  Set up the arguments for printf
+	mov rdi, format
+	mov rsi, hello_msg
+	xor rax, rax  	; Clear RAX to indicate no floating-point arguments (RAX=0)
+
+	;;  Call printf
+	call printf
+
+	;;  Exit the program
+	xor rdi, rdi
+	call exit
+
+	section .bss
+	;;  Reserve some space for the printf function
+	resb 8
+	
