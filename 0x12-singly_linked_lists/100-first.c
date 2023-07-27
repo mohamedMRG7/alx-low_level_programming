765f5597560ebe122d5a488ce71193d8143d90f1
mohamedMRG <mohamad1325@gmail.com>
2023-07-27 14:27:49 -0700

    task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+/**
+ * print_message - d
+ */
 void __attribute__((constructor)) print_message(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+I bore my house upon my back!\n");
 }
