9ee8353b8808ef940296eee678b9ca753feb313b
mohamedMRG <mohamad1325@gmail.com>
2023-07-31 05:38:53 -0700

    task

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+* list_len - check the code
+* @h:h
+* Return: Always 0.
+*/
+size_t listint_len(const listint_t *h)
+{
+	unsigned int i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
