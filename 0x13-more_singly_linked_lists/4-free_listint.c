f1687f26cbac12ae7f904df077888bbfd36b1b35
mohamedMRG <mohamad1325@gmail.com>
2023-07-31 05:52:52 -0700

    task

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,12 +9,11 @@
 * @head:h
 * Return: Always 0.
 */
-void free_list(list_t *head)
+void free_listint(listint_t *head)
 {
 	if (head == NULL)
 		return;
 	if (head->next != NULL)
-		free_list(head->next);
-	free(head->str);
+		free_listint(head->next);
 	free(head);
 }
