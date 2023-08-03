870a29f268cd3ff4b7d08af1739236cb166dd5a3
mohamedMRG <mohamad1325@gmail.com>
2023-08-03 11:19:21 -0700

    task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,9 +11,22 @@
 */
 void print_binary(unsigned long int n)
 {
-	if (n > 0)
-	{
-		print_binary(n / 2);
-		_putchar(n % 2 > 0 ? '1' : '0');
-	}
+    int shift = 63;  // Assuming 64-bit long int (change if using different size)
+    int bit;
+
+    if (n == 0)
+    {
+        _putchar('0');
+        return;
+    }
+
+    while (!(n >> shift) && shift > 0)
+        shift--;
+
+    while (shift >= 0)
+    {
+        bit = (n >> shift) & 1;
+        _putchar(bit + '0');
+        shift--;
+    }
 }
