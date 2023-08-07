c25de7b37806746d6dfcb36fd8bd3974d2f792f5
mohamedMRG <mohamad1325@gmail.com>
2023-08-07 14:09:21 -0700

    task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - check the code
+ * @filename:f
+ * @text_content:s
+ * Return: Always 0.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fb;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fb = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	if (fb == -1)
+	{
+		printf("Faild to open");
+		return (-1);
+	}
+	bytes_written = write(fb, text_content, strlen(text_content));
+	if (bytes_written == -1)
+	{
+		printf("Faild to write");
+		close(fb);
+		return (-1);
+	}
+	close(fb);
+	return (1);
+}
