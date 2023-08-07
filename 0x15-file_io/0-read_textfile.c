7e4b0daf5b14587910388e7695b97b82adc5dfec
mohamedMRG <mohamad1325@gmail.com>
2023-08-07 08:35:34 -0700

    task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fb;
+    char *buffer;
+    ssize_t read_buff;
+
+    if (filename == NULL)
+    {
+        return (0);
+    }
+    buffer = malloc(sizeof(buffer) * letters);
+    if (buffer == NULL)
+    {
+        return (0);
+    }
+    fb = open(filename,O_RDONLY);
+    if (fb == -1)
+    {
+        return (0);
+    }
+    read_buff = read(fb, buffer, letters);
+    write(STDOUT_FILENO, buffer, read_buff);
+    if (read_buff == -1)
+    {
+        return (0);
+    }
+    return (read_buff);
+}
