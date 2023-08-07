9a2bf7cb88a7616181e5294f4ad2a7f17e14a640
mohamedMRG <mohamad1325@gmail.com>
2023-08-07 14:39:19 -0700

    task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,12 +14,10 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-
-
 	int fd;
 	ssize_t bytes_written;
 	ssize_t text_len;
-	
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
