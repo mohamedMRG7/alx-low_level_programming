1653e52d0f805692da314cc2912488f8323fb88b
mohamedMRG <mohamad1325@gmail.com>
2023-08-07 14:34:48 -0700

    task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
 #include <unistd.h>
+
 
 /**
  * append_text_to_file - Appends text at the end of a file.
@@ -11,13 +14,14 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
-		return (-1);
+
 
 	int fd;
 	ssize_t bytes_written;
 	ssize_t text_len;
-
+	
+	if (filename == NULL)
+		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
--- 0x15-file_io/run.c
+++ 0x15-file_io/run.c
@@ -11,8 +11,12 @@ int main(int ac, char **av)
 {
     int res;
 
-    (void)ac;
-    res = create_file(av[1], av[2]);
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = append_text_to_file(av[1], av[2]);
     printf("-> %i)\n", res);
     return (0);
 }
