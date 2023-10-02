2993eb56ba3c84ac41e12efdc2ad1ce28d61ddb5
mohamed ragab <mohamad1325@gmail.com>
2023-10-02 13:45:09 +0300

    fileio

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <elf.h>
+
+/**
+ * print_magic - Prints the ELF magic numbers.
+ * @e_ident: The ELF identification array.
+ */
+void print_magic(unsigned char e_ident[EI_NIDENT])
+{
+	int i;
+
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x%c", e_ident[i], i == EI_NIDENT - 1 ? '\n' : ' ');
+}
+
+/**
+ * print_elf_header - Displays the ELF header information.
+ * @header: The ELF header.
+ */
+void print_elf_header(Elf64_Ehdr header)
+{
+	printf("ELF Header:\n");
+	print_magic(header.e_ident);
+	printf("  Class:                             ");
+	switch (header.e_ident[EI_CLASS])
+	{
+	case ELFCLASSNONE: printf("No ELF class\n"); break;
+	case ELFCLASS32:   printf("ELF32\n");       break;
+	case ELFCLASS64:   printf("ELF64\n");       break;
+	default:           printf("<unknown>\n");  break;
+	}
+	printf("  Data:                              ");
+	switch (header.e_ident[EI_DATA])
+	{
+	case ELFDATANONE: printf("Unknown data format\n"); break;
+	case ELFDATA2LSB: printf("2's complement, little endian\n"); break;
+	case ELFDATA2MSB: printf("2's complement, big endian\n"); break;
+	default:          printf("<unknown>\n"); break;
+	}
+	printf("  Version:                           %d (current)\n", header.e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            ");
+	switch (header.e_ident[EI_OSABI])
+	{
+	case ELFOSABI_SYSV:     printf("UNIX - System V\n"); break;
+	case ELFOSABI_NETBSD:   printf("UNIX - NetBSD\n");   break;
+	case ELFOSABI_SOLARIS:  printf("UNIX - Solaris\n");  break;
+	default:                printf("<unknown: %d>\n", header.e_ident[EI_OSABI]); break;
+	}
+	printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
+	printf("  Type:                              ");
+	switch (header.e_type)
+	{
+	case ET_NONE:   printf("No file type\n");   break;
+	case ET_REL:    printf("REL (Relocatable file)\n"); break;
+	case ET_EXEC:   printf("EXEC (Executable file)\n"); break;
+	case ET_DYN:    printf("DYN (Shared object file)\n"); break;
+	case ET_CORE:   printf("CORE (Core file)\n"); break;
+	default:        printf("<unknown>\n"); break;
+	}
+	printf("  Entry point address:               0x%lx\n", header.e_entry);
+}
+
+int main(int argc, char *argv[])
+{
+
+
+	Elf64_Ehdr header;
+	ssize_t bytes_read;
+	int fd;
+	
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
+		exit(98);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Cannot open %s\n", argv[1]);
+		exit(98);
+	}
+	bytes_read = read(fd, &header, sizeof(header));
+	if (bytes_read != sizeof(header) || header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 || header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		dprintf(STDERR_FILENO, "Error: %s is not an ELF file\n", argv[1]);
+		close(fd);
+		exit(98);
+	}
+
+	print_elf_header(header);
+
+	close(fd);
+	return (0);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: The name of the file.
+ * @text_content: The NULL terminated string to add at the end of the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+	ssize_t text_len;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		text_len = strlen(text_content);
+		bytes_written = write(fd, text_content, text_len);
+
+		close(fd);
+
+		if (bytes_written == text_len)
+			return (1);
+		else
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
